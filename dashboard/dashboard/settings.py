f4897defb4e2404214f0bfb4205bbadf1ef50e20
aashishg07 <ash.gtm07@gmail.com>
2023-05-12 17:06:54 +0545

    updated env file

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -176,13 +176,13 @@ DJOSER = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ash.gtm07@gmail.com'
-EMAIL_HOST_PASSWORD = 'byzcwynicawpbhju'
-DEFAULT_FROM_EMAIL = 'ash.gtm07@gmail.com'
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
 
 from datetime import timedelta
